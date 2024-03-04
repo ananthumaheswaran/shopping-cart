231571a67ad0d1dda623c08ccf9bc533dc450aee
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2024-03-04 22:08:13 +0530

    NavBar.jsx imported

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,18 @@
+import NavBar from "../components/NavBar";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <NavBar />
+      <div className="home-image-container">
+        <img
+          src="src\assets\images\medium-shot-young-woman-with-smartphone.jpg"
+          alt="clothes image"
+          className="home-image"
+        />
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
