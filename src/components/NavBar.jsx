9adfe6d256207abe52502e5222b08c4f19c850f7
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2024-03-04 22:11:28 +0530

    Added svg icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,20 @@
 const NavBar = () => {
   return (
-    <div>
+    <>
       <div className="navbar-container">
         <header className="title-block">
-          <h1>ShopNest</h1>
+          <h1>FakeStore</h1>
         </header>
         <nav className="nav-block">
           <ul className="nav-list">
-            <li className="nav-list-item">Mens</li>
-            <li className="nav-list-item">Kids</li>
-            <li className="nav-list-item">Womens</li>
+            <li className="nav-list-item">Men</li>
+            <li className="nav-list-item">Women</li>
             <li className="nav-list-item">Accessories</li>
             <li className="nav-list-item">Shop</li>
             <li className="nav-list-item">About Us</li>
           </ul>
         </nav>
+        {/* <div className="search-bar"> </div> */}
         <div className="cart-icon">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -27,12 +27,12 @@ const NavBar = () => {
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
             />
           </svg>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
