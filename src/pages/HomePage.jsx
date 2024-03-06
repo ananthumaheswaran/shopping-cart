8f46e6ed7b9b5a540f86a4c9e06ff9eb771b94e0
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2024-03-06 10:06:47 +0530

    Removes img element and added p element

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,24 @@
+import FeaturedProducts from "../components/FeaturedProducts";
 import NavBar from "../components/NavBar";
 
 const HomePage = () => {
   return (
     <>
       <NavBar />
+
       <div className="home-image-container">
-        <img
-          src="src\assets\images\medium-shot-young-woman-with-smartphone.jpg"
-          alt="clothes image"
-          className="home-image"
-        />
+        <h1 className="title-name">FakeStore</h1>
+        <p className="title-para">
+          A fictional online store showcasing diverse products with fake data
+          <br />
+          generated via Fake Store API, offering a simulated shopping
+          experience,
+          <br />
+          ideal for practicing web development and API integration.
+        </p>
+        <button className="btn-shop">Shop</button>
       </div>
+      <FeaturedProducts />
     </>
   );
 };
