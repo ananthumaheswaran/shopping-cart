a821f699bab44b71418627a92d15db5e956325c9
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2024-03-06 20:24:13 +0530

    Added background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        img: "url('./src/assets/images/medium-shot-young-woman-with-smartphone.jpg')",
+      }),
+    },
   },
   plugins: [],
 };
