23967a5ce3f69c85329a4b477fb6902f11c5b17b
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2024-03-08 21:49:50 +0530

    Path added for Shop.jsx

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import ErrorPage from "../pages/ErrorPage.jsx";
+import Shop from "../pages/Shop.jsx";
 
 const Router = () => {
   const router = createBrowserRouter([
@@ -8,6 +9,11 @@ const Router = () => {
       path: "/",
       element: <App />,
       errorElement: <ErrorPage />,
+    },
+
+    {
+      path: "shop",
+      element: <Shop />,
     },
   ]);
   return <RouterProvider router={router} />;
