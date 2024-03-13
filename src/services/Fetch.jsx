415bc2fd190b631951da5ab85a8969bf5b382736
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2024-03-13 21:04:19 +0530

    Component restructured

--- src/services/Fetch.jsx
+++ src/services/Fetch.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 const Fetch = () => {
   const [products, setProducts] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -16,28 +16,61 @@ const Fetch = () => {
       })
       .then((data) => {
         setProducts(data);
-        setLoading(false);
+        setIsLoading(false);
       })
       .catch((error) => {
         setError(error.message);
-        setLoading(false);
+        setIsLoading(false);
       });
   }, []);
 
   if (error) return <p>A network error was encountered</p>;
-  if (loading) return <p>Loading...</p>;
+  if (isLoading)
+    return (
+      <>
+        <button type="button" class=" ..." disabled>
+          {/* <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"> */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="animate-spin w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+            />
+          </svg>
+          {/* </svg> */}
+        </button>
+      </>
+    );
   if (!products) return <p>No products found</p>;
   return (
-    <div>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h2>{product.title}</h2>
-          <p>{product.description}</p>
-          <p>${product.price}</p>
-          <img src={product.image} alt={product.title} />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="product-card-container">
+        {products.map((product) => (
+          <div className="product-card">
+            <div key={product.id}>
+              <img
+                src={product.image}
+                alt={product.title}
+                className="product-images"
+              />
+              <div className="product-details">
+                <p className="product-title">{product.title}</p>
+                {/* <p>{product.description}</p> */}
+                <p className="product-price">${product.price}</p>
+                {/* <p>{product.category}</p> */}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
