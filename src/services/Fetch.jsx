306cd74a470344e97f6346d8caf9d760c8e8e6e8
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2024-03-20 23:38:33 +0530

    Removed unwanted tags

--- src/services/Fetch.jsx
+++ src/services/Fetch.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import ErrorPage from "../pages/ErrorPage";
 
 const Fetch = () => {
   const [products, setProducts] = useState(null);
@@ -24,12 +25,11 @@ const Fetch = () => {
       });
   }, []);
 
-  if (error) return <p>A network error was encountered</p>;
+  if (error) return <ErrorPage />;
   if (isLoading)
     return (
       <>
-        <button type="button" class=" ..." disabled>
-          {/* <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"> */}
+        <div className="loading-screen-icon">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -44,8 +44,7 @@ const Fetch = () => {
               d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
             />
           </svg>
-          {/* </svg> */}
-        </button>
+        </div>
       </>
     );
   if (!products) return <p>No products found</p>;
@@ -53,22 +52,17 @@ const Fetch = () => {
     <>
       <div className="product-card-container">
         {products.map((product) => (
-          <div className="product-card">
-            <div key={product.id}>
-              <div className="product-images-container">
-                <img
-                  src={product.image}
-                  alt={product.title}
-                  className="product-images"
-                />
-              </div>
-
-              <div className="product-details-container">
-                <p className="product-title">{product.title}</p>
-                {/* <p>{product.description}</p> */}
-                <p className="product-price">${product.price}</p>
-                {/* <p>{product.category}</p> */}
-              </div>
+          <div className="product-card" key={product.id}>
+            <div className="product-images-container">
+              <img
+                src={product.image}
+                alt={product.title}
+                className="product-images"
+              />
+            </div>
+            <div className="product-details-container">
+              <p className="product-title">{product.title}</p>
+              <p className="product-price">${product.price}</p>
             </div>
           </div>
         ))}
