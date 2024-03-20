de184fa7d001fb5da0a4942d5381f4984161de19
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2024-03-20 23:37:33 +0530

    class name added

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
+    <div className="error-message">
       <h1>Oh no, this route doesn't exist!</h1>
       <ul>
         <li>
