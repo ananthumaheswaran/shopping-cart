fb2eb9203176d4f7ca5f0f0606104baae1db2f5e
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2024-03-27 23:57:54 +0530

    Classname added to return link element

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -4,7 +4,7 @@ const ErrorPage = () => {
   return (
     <div className="error-message">
       <h1>Oh no, this route doesn't exist!</h1>
-      <ul>
+      <ul className="returnLinkToHomepage">
         <li>
           <Link to="/">
             You can go back to the home page by clicking here, though!
