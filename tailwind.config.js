077aeeabcbcc5bb3fa0a1be6fbbc4df9a20cdca6
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2024-03-30 23:53:16 +0530

    Added one more background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       backgroundImage: (theme) => ({
         img1: "url('./src/assets/images/medium-shot-young-woman-with-smartphone.jpg')",
         img2: "url('./src/assets/images/yellow-van-shipping.jpg')",
+        img3: "url(./src/assets/images/image-beautiful-woman-sitting-home-listening-music.jpg)",
       }),
     },
   },
